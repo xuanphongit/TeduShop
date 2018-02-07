1c3d602228f8b79f83f3cc63c298257b8afa8a77
Mai Xuân Phong <xuanphongit1109@gmail.com>
2018-02-07 19:46:23 +0700

    Add model

---
+++ TeduShop.Model/Models/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Url { get; set; }
+        public int? DisplayOrder { get; set; }
+        [Required]
+        public int GroupId { get; set; }
+        [ForeignKey("GroupId")]
+        public virtual MenuGroup MenuGroup { get; set; }
+        [Required]
+        public string Target { get; set; }
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual IEnumerable<Menu> Menus { get; set; }
+    }
+}
